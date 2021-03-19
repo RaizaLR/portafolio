1a938c952e893f3caa90f0a6ffcb1515f4c1aa3c
RaizaLR <raizalongart@gmail.com>
2021-03-18 23:56:25 -0300

    añadido contenido de proyectos

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,34 @@
+import TuOportunidad from './TuOportunidad.svg'
+import SushiQueen from './SushiQueen.svg'
+import BeautyTips from './BeautyTips.svg'
+import githubIcon from './githubIcon.svg'
 function Projects() {
     return (
         <div>
-            <h1>Proyectos</h1>
-           <p>Proyecto 1</p> 
-           <p>Proyecto 2</p> 
-           <p>Proyecto 3</p> 
-           <p>Proyecto 4</p> 
+            <div className='projects'>
+                    <h1 className='projectsTitle'>Proyectos</h1>
+                    <div className='leftProjectContainer'>
+                        <img src={TuOportunidad} alt='Tu Oportunidad'/>
+                        <div className='leftButtonscontainer'>
+                            <span className='demoBtn'>Ver demo</span>
+                            <span className='githubBtn'><img src={githubIcon} alt='GitHub' className='githubLogo'/>GitHub</span>
+                        </div>
+                    </div>
+                    <div className='rightProjectContainer'>
+                        <div className='rightButtonscontainer'>
+                            <span className='demoBtn'>Ver demo</span>
+                            <span className='githubBtn'><img src={githubIcon} alt='GitHub' className='githubLogo'/>GitHub</span>
+                        </div>
+                        <img src={SushiQueen} alt='Sushi Queen'/>
+                    </div>
+                    <div className='leftProjectContainer'>
+                        <img src={BeautyTips} alt='Beauty Tips'/>
+                        <div className='leftButtonscontainer'>
+                            <span className='demoBtn'>Ver demo</span>
+                            <span className='githubBtn'><img src={githubIcon} alt='GitHub' className='githubLogo'/>GitHub</span>
+                        </div>
+                    </div>
+            </div>
         </div>
     )}
 

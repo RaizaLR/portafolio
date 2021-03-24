d4361624fd564925bcfa84c8eae051f34a6076c1
RaizaLR <raizalongart@gmail.com>
2021-03-24 11:25:47 -0300

    añadido redireccionamiento a links a seccion de contacto

--- src/Contact.js
+++ src/Contact.js
@@ -2,8 +2,12 @@ import ContactImg from './ContactImg.svg'
 import MailImg from './MailImg.svg'
 import LinkedInLogo from './LinkedInLogo.svg'
 import GitHubLogo from './GitHubLogo.svg'
+import LogoLaboratoria from './LogoLaboratoria.svg'
 
-
+// funcion para abrir links
+const openLink = (link) => {
+    window.open(link)
+}
 
 function Contact() {
     return (
@@ -16,15 +20,19 @@ function Contact() {
                 <div>
                     <div className='contactInfo'>
                         <img src={MailImg} alt='Gmail' className='logo'/>
-                        <p>Raizalongart@gmail.com</p>
+                        <a href="mailto:raizalongart@gmail.com"><p className="linkToClick">Raizalongart@gmail.com</p></a>
                     </div>
                     <div className='contactInfo'>
                         <img src={LinkedInLogo} alt='linkedin' className='logo'/>
-                        <p>Linkedin.com/in/raizalongart</p>
+                        <p className="linkToClick" onClick={() => {openLink('https://linkedin.com/in/raizalongart')}}>Linkedin.com/in/raizalongart</p>
                     </div>
                     <div className='contactInfo'>
                         <img src={GitHubLogo} alt='gitHub' className='logo'/>
-                        <p>Github.com/RaizaLR</p>
+                        <p className="linkToClick" onClick={() => {openLink('https://github.com/RaizaLR')}}>Github.com/RaizaLR</p>
+                    </div>
+                    <div className='contactInfo'>
+                        <img src={LogoLaboratoria} alt='appTalentoLab' className='logo'/>
+                        <p className="linkToClick" onClick={() => {openLink('https://app.talento.laboratoria.la/profile/zLSo6EUimqbOp5ZzLtGNawI3QFD2')}}>app.talento.laboratoria.la</p>
                     </div>
                 </div>
             </div>
